d38f9e65b547d27efe1bd81a6dfad09c86c4ddd0
Neilos <nosniktatkinson@hotmail.co.uk>
2014-05-15 12:58:15 +0100

    Move all angular config and stuff to main.’s
    (also correct the location of the Stock service - should be in the services directory)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,6 @@
 //= require angular
 //= require angular-mocks
 //= require angular-resource
-//= require_self
 //= require main
 //= require_tree ./angular_app_files
 //= require_tree .
-
-// make angular work with asset pipeline
-$(document).on('page:load', function () {
-  $('[ng-app]').each(function () {
-    module = $(this).attr('ng-app');
-    angular.bootstrap(this, [module]);
-  });
-});
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,3 +4,11 @@ app.config(["$httpProvider", function($httpProvider) {
   csrfToken = $('meta[name=csrf-token]').attr('content');
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 }]);
+
+// make angular work with turbolinks
+$(document).on('page:load', function () {
+  $('[ng-app]').each(function () {
+    module = $(this).attr('ng-app');
+    angular.bootstrap(this, [module]);
+  });
+});
