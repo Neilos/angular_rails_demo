83375374919a0ebef642f891c7e8e75d8861a70d
Neilos <nosniktatkinson@hotmail.co.uk>
2014-05-19 11:58:34 +0100

    Revert "CRSF with Angular - METHOD 3 (angular httpProvider configuration)"
    This reverts commit f56358cb4d369cdafb3d384e3efa3107b9a11c58.
    Revert to using server side crsf fix. Adjusting the headers client side (with angular) caused problems with cross origin policy and CORS when accessing 3rd party apis with javascript. The presence of additional header with a crsf token was not expected by the 3rd party api (yahoo in this case) and so the request failed.
    
    Conflicts:
    	app/assets/javascripts/main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,5 @@
 var app = angular.module('StockWatch', ['ngResource']);
 
-app.config(["$httpProvider", function($httpProvider) {
-  csrfToken = $('meta[name=csrf-token]').attr('content');
-  $httpProvider.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-}]);
-
 // make angular work with turbolinks
 $(document).on('page:load', function () {
   $('[ng-app]').each(function () {
