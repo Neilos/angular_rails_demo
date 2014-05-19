67200299246e1d655436f5665da8f8321ff1b771
Neilos <nosniktatkinson@hotmail.co.uk>
2014-05-19 13:25:29 +0100

    Add loading message.

--- app/assets/javascripts/angular_app_files/controllers/stocks_ctrl.js
+++ app/assets/javascripts/angular_app_files/controllers/stocks_ctrl.js
@@ -9,9 +9,11 @@ app.controller('StockCtrl', ['$scope', 'Stock', '$filter', '$http', function($sc
   $scope.createStock = function() {
     var attr ={};
     attr.symbol = $filter('uppercase')($scope.newCompany);
+    $scope.loading = true;
     $http({method: 'GET', url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D" + "'" + attr.symbol + "'" + "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     }).success(function(data, status, headers, config){
       $scope.error = false;
+      $scope.loading = false;
       attr.name = data.query.results.quote["Name"];
       attr.bid = data.query.results.quote["Bid"];
       attr.ask = data.query.results.quote["Ask"];
@@ -23,6 +25,7 @@ app.controller('StockCtrl', ['$scope', 'Stock', '$filter', '$http', function($sc
       $scope.newSymbol = "";
     }).error(function(data, status, headers, config){
       $scope.error = true;
+      $scope.loading = false;
     })
   };
 
